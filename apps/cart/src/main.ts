3eed724a479695c8d2a756cd80ba85b2585b6b6c
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2026-06-07 22:07:15 +0300

    feat(apps): convert starter app to monorepo

---
+++ apps/cart/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { CartModule } from './cart.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(CartModule);
+  await app.listen(process.env.port ?? 3000);
+}
+
+void bootstrap();
---
+++ apps/products/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { ProductsModule } from './products.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(ProductsModule);
+  await app.listen(process.env.port ?? 3000);
+}
+
+void bootstrap();
