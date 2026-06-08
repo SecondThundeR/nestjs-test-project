03ce6fc10806e96739a42778b5babe13d9e74398
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2026-06-08 23:10:40 +0300

    feat(gateway): remove `x-powered-by` header

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
 import { type ServicesConfig, servicesConfig } from '@app/contracts';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { type NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(GatewayModule);
+  const app = await NestFactory.create<NestExpressApplication>(GatewayModule);
 
   const services = app.get<ServicesConfig>(servicesConfig.KEY);
 
+  app.disable('x-powered-by');
   app.setGlobalPrefix('api');
   app.enableCors();
   app.useGlobalPipes(
